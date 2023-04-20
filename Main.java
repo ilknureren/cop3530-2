8033fe2e6b1c5ff0013183ee3ef8d10008a52ae0
Minoudjie <100977606+Minoudjie@users.noreply.github.com>
2023-04-20 18:15:21 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,20 +2,28 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        CityPathGraph cityGraph = new CityPathGraph();
-        cityGraph.addCity("New York");
-        cityGraph.addCity("Chicago");
-        cityGraph.addCity("San Francisco");
-        cityGraph.addCity("Los Angeles");
-        cityGraph.addConnection("New York", "Chicago");
-        cityGraph.addConnection("New York", "San Francisco");
-        cityGraph.addConnection("Chicago", "Los Angeles");
-        cityGraph.addConnection("San Francisco", "Los Angeles");
-        List<String> path = cityGraph.getPath("New York", "Los Angeles");
-        if (path == null) {
-            System.out.println("No path found.");
-        } else {
-            System.out.println("Path: " + path);
+        Graph graph = new Graph();
+        City sanFrancisco = new City("San Francisco", 37, -122);
+        City newYork = new City("New York", 34, -118);
+        City chicago = new City("Chicago", 20, -20);
+        City losAngeles = new City("Los Angeles", 14, -300);
+        City miami = new City("Miami", 50, -250);
+        graph.addCity(sanFrancisco);
+        graph.addCity(newYork);
+        graph.addCity(chicago);
+        graph.addCity(losAngeles);
+        graph.addCity(miami);
+        graph.addEdge(new Edge(sanFrancisco, newYork, 383));
+        graph.addEdge(new Edge(sanFrancisco, chicago, 2112));
+        graph.addEdge(new Edge(losAngeles, chicago, 2015));
+        graph.addEdge(new Edge(newYork, chicago, 790));
+        graph.addEdge(new Edge(newYork, miami, 1306));
+        graph.addEdge(new Edge(losAngeles, miami, 2786));
+
+        ArrayList<City> path = PathFinder.findShortestPath(graph, sanFrancisco, miami);
+        System.out.println("Shortest path from " + sanFrancisco.getName() + " to " + miami.getName() + ":");
+        for (City city : path) {
+            System.out.println(city.getName());
         }
     }
 }
@@ -85,6 +93,9 @@ class Graph {
 }
 
 class PathFinder {
+    public PathFinder(CityGraph cityGraph) {
+    }
+
     public static ArrayList<City> findShortestPath(Graph graph, City source, City destination) {
         Map<City, Integer> distance = new HashMap<>();
         Map<City, City> previous = new HashMap<>();
@@ -128,5 +139,9 @@ class PathFinder {
         Collections.reverse(path);
 
         return path;
+    }
+
+    public List<String> findPath(String startCity, String endCity) {
+        return null;
     }
 }
