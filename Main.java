632e32063573ac0578cf91951a094e3a4f05224b
Ilknur Eren <ilkxeren@gmail.com>
2023-04-20 19:40:41 -0400

    update main

--- Main.java
+++ Main.java
@@ -27,6 +27,32 @@ public class Main {
         }
     }
 }
+
+class CityGraph {
+    private Map<String, List<String>> adjacencyList;
+
+    public CityGraph() {
+        adjacencyList = new HashMap<>();
+    }
+
+    public void addVertex(String vertex) {
+        adjacencyList.put(vertex, new ArrayList<>());
+    }
+
+    public void addEdge(String vertex1, String vertex2) {
+        adjacencyList.get(vertex1).add(vertex2);
+        adjacencyList.get(vertex2).add(vertex1);
+    }
+
+    public List<String> getVertices() {
+        return new ArrayList<>(adjacencyList.keySet());
+    }
+
+    public List<String> getNeighbors(String vertex) {
+        return new ArrayList<>(adjacencyList.get(vertex));
+    }
+}
+
 class City {
     private String name;
     private int latitude;
